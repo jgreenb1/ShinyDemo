5288f91706de9156fac64e6f2dc19020ed50e7b2
jgreenb1 <greenberg.jon@gmail.com>
2015-03-25 12:14:02 -0700

    Shiny Demo Versions 3 and 4

---
+++ Version3.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+ui<-fluidPage(
+	h1("I am Jon"),
+	sidebarLayout(
+		sidebarPanel(
+			selectInput("variable1","Choose a dataset:",
+				choices=ls(package:datasets),
+				selected="pressure")),
+		mainPanel(
+			verbatimTextOutput("dump"),
+			plotOutput("plot"),
+			tableOutput("table"))
+		)
+)
+
+server<-function(input,output,session)
+	{
+	output$dump<-renderPrint({
+		dataset<-get(input$variable1,"package:datasets",inherit=FALSE)
+		str(dataset)})
+
+	output$plot<-renderPlot({
+		dataset<-get(input$variable1,"package:datasets",inherit=FALSE)
+		plot(dataset)})
+
+	output$table<-renderTable({
+		dataset<-get(input$variable1,"package:datasets",inherit=FALSE)
+		dataset})
+	}
+
+shinyApp(ui,server)
